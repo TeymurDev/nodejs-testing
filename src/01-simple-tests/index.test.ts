b277d973ecbafe812f5f6f2058fe8e39e92c4c33
TeymurDev <teymur.ff@gmail.com>
2024-02-10 16:11:24 +0400

    feat: add simple tests

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,25 +1,34 @@
 // Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    const result = simpleCalculator({ a: 6, b: 2, action: Action.Add });
+    expect(result).toBe(8);
   });
 
   test('should subtract two numbers', () => {
-    // Write your test here
+    const result = simpleCalculator({ a: 6, b: 2, action: Action.Subtract });
+    expect(result).toBe(4);
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
+    const result = simpleCalculator({ a: 6, b: 2, action: Action.Multiply });
+    expect(result).toBe(12);
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
+    const result = simpleCalculator({ a: 6, b: 2, action: Action.Divide });
+    expect(result).toBe(3);
   });
 
   test('should exponentiate two numbers', () => {
-    // Write your test here
+    const result = simpleCalculator({
+      a: 6,
+      b: 2,
+      action: Action.Exponentiate,
+    });
+    expect(result).toBe(36);
   });
 
   test('should return null for invalid action', () => {
